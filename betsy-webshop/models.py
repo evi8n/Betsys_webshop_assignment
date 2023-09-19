54cf59f59c5870f6d15c5a5d8f5894a7c595ed0e
evi8n <129991219+evi8n@users.noreply.github.com>
2023-09-19 21:58:54 +0200

    Update models.py

--- betsy-webshop/models.py
+++ betsy-webshop/models.py
@@ -9,7 +9,7 @@ class BaseModel(peewee.Model):
         database = db
 
 
-class User(BaseModel):
+class Buyer(BaseModel):
     username = peewee.CharField(unique=True, max_length=20)
     name = peewee.CharField(max_length=20)
     address = peewee.CharField(max_length=30)
@@ -21,25 +21,28 @@ class Tag(BaseModel):
 
 
 class Product(BaseModel):
-    name = peewee.CharField(max_length=20, default="")
-    description = peewee.CharField(max_length=100)
+    product_name = peewee.CharField(max_length=100, default="", index=True)
+    description = peewee.CharField(max_length=100, index=True)
     price = peewee.DecimalField(max_digits=10, decimal_places=2)
     quantity_in_stock = peewee.IntegerField(
         constraints=[peewee.Check("quantity_in_stock >= 0")]
     )
-    owners = peewee.ManyToManyField(User, backref="owned_products")
+    owners = peewee.ManyToManyField(Buyer, backref="owned_products")
     tags = peewee.ManyToManyField(Tag, backref="product_tag")
 
 
 class Transaction(BaseModel):
-    user = peewee.ForeignKeyField(User, backref="transaction_id")
-    product = peewee.ForeignKeyField(Product, backref="name")
+    user = peewee.ForeignKeyField(Buyer, backref="transaction_id")
+    product = peewee.ForeignKeyField(Product, backref="product_name", index=True)
     date = peewee.DateField()
     products_purchased = peewee.IntegerField(
         constraints=[peewee.Check("products_purchased >= 0")]
     )
 
 
+ProductBuyer = Product.owners.get_through_model()
+ProductTag = Product.tags.get_through_model()
+
 db.connect()
 
-db.create_tables([User, Product, Transaction, Tag])
+db.create_tables([Buyer, Product, Transaction, Tag, ProductBuyer, ProductTag])
